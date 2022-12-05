d3dd09f6b0421fcdded690d971d8fd28f45f6b18
Артём <116555806+ArtemKhasanshin-QA@users.noreply.github.com>
2022-12-05 12:29:16 +0300

    Update TestАвторизации.js

--- TestАвторизации.js
+++ TestАвторизации.js
@@ -2,8 +2,8 @@ describe ('Тестирование авторизации на staya', function
    it('Проверка сайта на авторизацию', function () {
       cy.visit('https://staya.dog/');
       cy.get('.header-bottom__right--link').click();
-      cy.get('.auth-form > form > [type="email"]').type('dscksj@bk.com');
-      cy.get('.auth-form > form > [type="password"]').type('esfjslef.eslfj');
+      cy.get('.auth-form > form > [type="email"]').type('Ввод логина');
+      cy.get('.auth-form > form > [type="password"]').type('Ввод пароля');
       cy.get('.auth-form__submit').click();
       cy.contains('Мои заказы');
    })
